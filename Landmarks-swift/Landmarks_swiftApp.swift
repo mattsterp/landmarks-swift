1bf316c8f7e4b6eaac0c553a4b44df3e47dbda19
Matthhew Pters <mattsterp@gmail.com>
2021-04-30 15:27:56 -0400

    Initial Commit

---
+++ Landmarks-swift/Landmarks_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_swiftApp.swift
+//  Landmarks-swift
+//
+//  Created by Matthew Peters on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
